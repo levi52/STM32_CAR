5b95c103eb27b897a2438c9584a9ce3b9d7de37e
Levi5 <102035256+levi52@users.noreply.github.com>
2024-02-29 10:05:22 +0800

    Delete HARDWARE directory

--- HARDWARE/SPEED/speed.h
+++
@@ -1,9 +0,0 @@
-# ifndef __SPEED_H
-# define __SPEED_H
-#include "sys.h"
-
-void speed_init(void);
-uint16_t CountSensor_Get(void);
-uint16_t CountSensor1_Get(void);
-//float Speed_Get(void);
-#endif
